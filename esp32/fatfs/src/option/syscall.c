38e47f52149af9482529a87ca08b4c1d4e11d863
gauci <oligauc@pycom.io>
2017-01-20 09:23:39 +0100

    fatfs syscall cleanup

--- esp32/fatfs/src/option/syscall.c
+++ esp32/fatfs/src/option/syscall.c
@@ -7,6 +7,7 @@
 
 
 #if _FS_REENTRANT
+
 /*------------------------------------------------------------------------*/
 /* Create a Synchronization Object                                        */
 /*------------------------------------------------------------------------*/
@@ -20,25 +21,10 @@ int ff_cre_syncobj (	/* !=0:Function succeeded, ==0:Could not create due to any 
 	_SYNC_t *sobj		/* Pointer to return the created sync object */
 )
 {
-	int ret;
-
-//
-//	*sobj = CreateMutex(NULL, FALSE, NULL);		/* Win32 */
-//	ret = (int)(*sobj != INVALID_HANDLE_VALUE);
-
-//	*sobj = SyncObjects[vol];			/* uITRON (give a static created sync object) */
-//	ret = 1;							/* The initial value of the semaphore must be 1. */
-
-//	*sobj = OSMutexCreate(0, &err);		/* uC/OS-II */
-//	ret = (int)(err == OS_NO_ERR);
-
 	vSemaphoreCreateBinary( (*sobj) );	/* FreeRTOS */
-	ret = (int)(*sobj != NULL);
-
-	return ret;
+	
+	return (int)(*sobj != NULL);
 }
-
-
 
 /*------------------------------------------------------------------------*/
 /* Delete a Synchronization Object                                        */
@@ -52,23 +38,10 @@ int ff_del_syncobj (	/* !=0:Function succeeded, ==0:Could not delete due to any 
 	_SYNC_t sobj		/* Sync object tied to the logical drive to be deleted */
 )
 {
-	int ret;
-
-
-//	ret = CloseHandle(sobj);	/* Win32 */
-
-//	ret = 1;					/* uITRON (nothing to do) */
-
-//	OSMutexDel(sobj, OS_DEL_ALWAYS, &err);	/* uC/OS-II */
-//	ret = (int)(err == OS_NO_ERR);
-
-    vSemaphoreDelete(sobj);		/* FreeRTOS */
-	ret = 1;
-
-	return ret;
+	vSemaphoreDelete(sobj);		/* FreeRTOS */
+	
+	return 1;
 }
-
-
 
 /*------------------------------------------------------------------------*/
 /* Request Grant to Access the Volume                                     */
@@ -81,21 +54,8 @@ int ff_req_grant (	/* 1:Got a grant to access the volume, 0:Could not get a gran
 	_SYNC_t sobj	/* Sync object to wait */
 )
 {
-	int ret;
-
-//	ret = (int)(WaitForSingleObject(sobj, _FS_TIMEOUT) == WAIT_OBJECT_0);	/* Win32 */
-
-//	ret = (int)(wai_sem(sobj) == E_OK);			/* uITRON */
-
-//	OSMutexPend(sobj, _FS_TIMEOUT, &err));		/* uC/OS-II */
-//	ret = (int)(err == OS_NO_ERR);
-
-	ret = (int)(xSemaphoreTake(sobj, _FS_TIMEOUT) == pdTRUE);	/* FreeRTOS */
-
-	return ret;
+	return (int)(xSemaphoreTake(sobj, _FS_TIMEOUT) == pdTRUE);
 }
-
-
 
 /*------------------------------------------------------------------------*/
 /* Release Grant to Access the Volume                                     */
@@ -107,21 +67,13 @@ void ff_rel_grant (
 	_SYNC_t sobj	/* Sync object to be signaled */
 )
 {
-//	ReleaseMutex(sobj);		/* Win32 */
-
-//	sig_sem(sobj);			/* uITRON */
-
-//	OSMutexPost(sobj);		/* uC/OS-II */
-
 	xSemaphoreGive(sobj);	/* FreeRTOS */
 }
 
 #endif
 
-
-
-
 #if _USE_LFN == 3	/* LFN with a working buffer on the heap */
+
 /*------------------------------------------------------------------------*/
 /* Allocate a memory block                                                */
 /*------------------------------------------------------------------------*/
